11baf1160463b3c076fb5c77b10757e11537646d
Son Pham-Ba <son.phamba@epfl.ch>
2025-08-05 16:12:46 +0200

    feat: frontend routes

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header bordered class="bg-white text-grey-10">
-      <app-toolbar @toggle-left="toggleLeftDrawer" @toggle-right="toggleRightDrawer" />
+      <app-toolbar/>
     </q-header>
 
     <q-drawer
@@ -10,9 +10,6 @@
       show-if-above
       bordered
     >
-      <div v-if="!miniStateLeft">
-        <layers-drawer />
-      </div>
       <div
         v-if="!$q.screen.lt.md"
         class="absolute"
@@ -28,47 +25,6 @@
       </div>
     </q-drawer>
 
-    <q-drawer
-      v-if="route.path === '/data-hub'"
-      v-model="rightDrawerOpen"
-      :mini="!rightDrawerOpen || miniStateRight"
-      :width="$q.screen.lt.md ? 300 : (showPlots ? 300 : 500)"
-      side="right"
-      show-if-above
-      bordered
-    >
-      <div v-show="!miniStateRight" class="q-mt-xl">
-        <div v-show="helpStore.show">
-          <help-drawer />
-          <div class="absolute" style="top: 10px; right: 20px">
-            <q-btn dense round unelevated icon="close" @click="toggleShowHelp" />
-          </div>
-        </div>
-        <div v-show="catalogStore.showStudyDetails">
-          <study-details-drawer />
-          <div class="absolute" style="top: 10px; right: 20px">
-            <q-btn dense round unelevated icon="close" @click="toggleShowStudyDetails" />
-          </div>
-        </div>
-        <div v-show="showPlots">
-          <plots-drawer />
-        </div>
-      </div>
-      <div
-        v-if="!$q.screen.lt.md"
-        class="absolute"
-        style="top: 10px; left: 10px"
-      >
-        <q-btn
-          dense
-          round
-          unelevated
-          :icon="miniStateRight ? 'chevron_left' : 'chevron_right'"
-          @click="miniStateRight = !miniStateRight"
-        />
-      </div>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -78,58 +34,14 @@
 <script setup lang="ts">
 import { useQuasar } from 'quasar';
 import AppToolbar from 'src/components/AppToolbar.vue';
-import LayersDrawer from 'src/components/LayersDrawer.vue';
-import PlotsDrawer from 'src/components/PlotsDrawer.vue';
-import HelpDrawer from 'src/components/HelpDrawer.vue';
-import StudyDetailsDrawer from 'src/components/StudyDetailsDrawer.vue';
 
 const $q = useQuasar();
-const helpStore = useHelpStore();
-const catalogStore = useCatalogStore();
 const route = useRoute();
 
 const leftDrawerOpen = ref(false);
-const rightDrawerOpen = ref(false);
 const miniStateLeft = ref(false);
-const miniStateRight = ref(false);
-
-const showPlots = computed(() => {
-  return !helpStore.show && !catalogStore.showStudyDetails;
+watch(() => route.meta.hasDrawer, (newValue) => {
+  leftDrawerOpen.value = (newValue === true);
 });
 
-watch(
-  () => helpStore.show,
-  (newValue) => {
-    if (newValue) {
-      rightDrawerOpen.value = true;
-      miniStateRight.value = false;
-    }
-  },
-);
-
-watch(
-  () => catalogStore.showStudyDetails,
-  (newValue) => {
-    if (newValue) {
-      rightDrawerOpen.value = true;
-      miniStateRight.value = false;
-    }
-  },
-);
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
-
-function toggleRightDrawer() {
-  rightDrawerOpen.value = !rightDrawerOpen.value;
-}
-
-function toggleShowHelp() {
-  helpStore.show = !helpStore.show;
-}
-
-function toggleShowStudyDetails() {
-  catalogStore.showStudyDetails = !catalogStore.showStudyDetails;
-}
 </script>
--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -5,6 +5,33 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
+      {
+        path: '',
+        component: () => import('pages/HomePage.vue'),
+      },
+      {
+        path: 'database',
+        component: () => import('pages/DatabasePage.vue'),
+        meta: { hasDrawer: true },
+      },
+      {
+        path: '/correlations',
+        component: () => import('pages/CorrelationsPage.vue'),
+        meta: { hasDrawer: true },
+      },
+      {
+        path: '/quality-index',
+        component: () => import('pages/QualityIndexPage.vue'),
+        meta: { hasDrawer: true },
+      },
+      {
+        path: '/others',
+        component: () => import('pages/OthersPage.vue'),
+      },
+      {
+        path: '/about',
+        component: () => import('pages/AboutPage.vue'),
+      },
     ],
   },
 
